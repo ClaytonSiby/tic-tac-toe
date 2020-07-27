01ad7691423f8d1fbc9010f4cbd4bc0e6368ccdc
ClaytonSiby <claytonsiby@gmail.com>
2020-07-27 12:34:40 +0200

    update board, player & game logic in main file

--- lib/board.rb
+++ lib/board.rb
@@ -1,40 +1,50 @@
+# frozen_string_literal: true
+
+require_relative '../lib/player'
+
+# board class definition
 class Board
-    @@positions = (1..9).to_a
+  @@positions = (1..9).to_a
+  def initialize
+    @@positions
+  end
 
-    def initialize
-        @@positions
-    end
+  def self.draw_board
+    "
+    #{@@positions[0]} | #{@@positions[1]} | #{@@positions[2]}
+    =======
+    #{@@positions[3]} | #{@@positions[4]} | #{@@positions[5]}
+    =======
+    #{@@positions[6]} | #{@@positions[7]} | #{@@positions[8]}
+    "
+  end
 
-    def draw_board
-        "
-            #{@@positions[0]} | #{@@positions[1]} | #{@@positions[2]}
-            =========
-            #{@@positions[3]} | #{@@positions[4]} | #{@@positions[5]}
-            =========
-            #{@@positions[6]} | #{@@positions[7]} | #{@@positions[8]}
-        "
-    end
+  def self.update_board
+    system('clear')
+    Board.draw_board
+  end
 
-    def self.error_message
-        'invalid move please enter a number between 1 and 9'
-    end
+  def self.valid_move?(move)
+    move >= 1 && move <= 9 ? true : false
+  end
 
-    def self.valid_move?(move)
-      @@positions.any?(move)
-    end
+  def self.position_empty?(move)
+    @@positions[move - 1].is_a?(Integer)
+  end
 
-    def self.position_empty?(move)
-         @@positions[move - 1].is_a?(String) ? "Position #{move} has been taken please make another choice" : true
-    end
+  def self.draw
+    'Game over & it is a draw!!' if @@positions.none?(Integer)
+  end
 
-    def self.asign_mark(move, mark)
-       return error_message unless valid_move?(move) && position_empty?(move)
+  def self.make_move(move, mark)
+    return 'Invalid move, enter a numbe in range 1..9' unless valid_move?(move)
 
-        @@positions[move - 1] = mark
-    end
+    return "Position #{move} has been taken, please select another. >> " unless position_empty?(move)
 
-    def self.board_full?
-        @@positions.none?(Integer)
-    end
+    @@positions[move - 1] = mark
+  end
 
+  def self.reset_board
+    @@positions.size.times { |el| @@positions[el] = el + 1 }
+  end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,27 @@
+# frozen_string_literal: true
+
+require_relative './lib/board.rb'
+
+# Player class definition
 class Player
-    attr_reader :name, :mark
-    def initialize(name, mark)
-        @name = name
-        @mark = mark
-    end
+  attr_reader :name, :mark, :choices
+  def initialize(name, mark)
+    @name = name
+    @mark = mark
+    @choices = []
+  end
+
+  def my_move(move)
+    return 'Invalid move, enter a number in range 1..9' unless Board.valid_move?(move)
+
+    store_move = Board.make_move(move, @mark)
+    return store_move unless store_move == @mark
+
+    @choices << move
+    move
+  end
+
+  def reset_choices
+    @choices = []
+  end
 end
