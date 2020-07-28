673898eca926daccd8ca68d422aa911f21f06ea4
Visvaldas <visva.rapalis@gmail.com>
2020-07-28 12:48:08 +0300

    Moved game logic blocks from main file to board file

--- lib/board.rb
+++ lib/board.rb
@@ -42,3 +42,24 @@ class Board
     end
   end
 end
+
+# determine if an array includes given elements.
+class Array
+  def contain_all?(arr)
+    arr.all? { |elem| include?(elem) }
+  end
+end
+
+# PlayGame module holding the game possible wins.
+module PlayGame
+  POSSIBLE_WINS = [
+    [1, 2, 3], [4, 5, 6],
+    [7, 8, 9], [1, 5, 9],
+    [2, 5, 8], [3, 5, 7],
+    [1, 4, 7], [3, 6, 9]
+  ].freeze
+
+  def self.won?(player)
+    POSSIBLE_WINS.any? { |elem| player.choices.contain_all?(elem) }
+  end
+end
