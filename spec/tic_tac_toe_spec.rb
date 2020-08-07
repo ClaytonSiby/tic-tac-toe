cf339db9cfdb21b684584c97ae3eb337b141c7cc
Yigit Mersin <yigitmersin@gmail.com>
2020-08-07 13:42:54 +0300

    Add test to check valid & invalid move

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -27,5 +27,18 @@ describe Array do
       expect(object.contain_all?(test_arr)).to eq(false) 
     end
   end
+end
 
+describe Board do
+  let(:move_invalid) {32}
+  let(:move_valid) {4}
+  let(:mark) {'X'}
+  
+  it "should prompt that move is invalid" do
+    expect(Board.make_move( move_invalid, mark)).to eq('Invalid move, enter a numbe in range 1..9')
+  end
+
+  it "should prompt X since move is in valid range(1..9)" do
+    expect(Board.make_move( move_valid, mark)).to eq('X')
+  end
 end
