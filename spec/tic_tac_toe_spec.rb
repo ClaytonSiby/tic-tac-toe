bf5aa7eaeb22bf8d5042d51ebf14841e22dc7db4
Yigit Mersin <yigitmersin@gmail.com>
2020-08-07 14:09:39 +0300

    Fix rubocop errors

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,9 +2,9 @@ require 'board'
 require 'player'
 
 describe Player do
-  subject(:instance) { Player.new("somename", "X") }
+  subject(:instance) { Player.new('somename', 'X') }
 
-  describe "#initialize" do
+  describe '#initialize' do
     it 'It should expect name instance values class as string' do
       expect(instance.name.class).to eq(String)
     end
@@ -12,44 +12,42 @@ describe Player do
     it 'It should expect mark instance values class as string' do
       expect(instance.mark.class).to eq(String)
     end
-
   end
-  
 end
 
 describe Array do
-  let(:object) {Array.new}
-  let(:test_arr) {Array.new}
-  describe "#contain_all?" do
+  let(:object) { [] }
+  let(:test_arr) { [] }
+  describe '#contain_all?' do
     it 'It compares object & argument arrays & return false if they are not same' do
-      object = [1,4,3]
-      test_arr = [1,2,3]
-      expect(object.contain_all?(test_arr)).to eq(false) 
+      object = [1, 4, 3]
+      test_arr = [1, 2, 3]
+      expect(object.contain_all?(test_arr)).to eq(false)
     end
   end
 end
 
 describe Board do
-  describe "#self.make_move" do
-    let(:move_invalid) {32}
-    let(:move_valid) {4}
-    let(:mark) {'X'}
-  
-    it "should prompt that move is invalid" do
-      expect(Board.make_move( move_invalid, mark)).to eq('Invalid move, enter a numbe in range 1..9')
+  describe '#self.make_move' do
+    let(:move_invalid) { 32 }
+    let(:move_valid) { 4 }
+    let(:mark) { 'X' }
+
+    it 'should prompt that move is invalid' do
+      expect(Board.make_move(move_invalid, mark)).to eq('Invalid move, enter a numbe in range 1..9')
     end
 
-    it "should prompt X since move is in valid range(1..9)" do
-      expect(Board.make_move( move_valid, mark)).to eq('X')
+    it 'should prompt X since move is in valid range(1..9)' do
+      expect(Board.make_move(move_valid, mark)).to eq('X')
     end
   end
-  
-  describe "self.draw_board" do
-  let(:positions) {Array.new}
+
+  describe 'self.draw_board' do
+    let(:positions) { [] }
     it "should display the board with user selection in 4 as 'X' " do
       positions = (1..9).to_a
       expect(Board.draw_board).to eq(
-         "
+        "
     #{positions[0]} | #{positions[1]} | #{positions[2]}
     =========
     X | #{positions[4]} | #{positions[5]}
@@ -60,4 +58,3 @@ describe Board do
     end
   end
 end
-
