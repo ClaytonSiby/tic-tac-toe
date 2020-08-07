f4e0c41b1342df6ac5131c396e946d867c43512b
Visvaldas <visva.rapalis@gmail.com>
2020-08-07 17:37:22 +0300

    Add test for my_move & update draw_board method test

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -13,6 +13,16 @@ describe Player do
       expect(instance.mark.class).to eq(String)
     end
   end
+
+  describe 'self.my_move' do
+    let(:object) {Player.new("Visvaldas" , "O")}
+    let(:move_valid) { 5 }
+    context "it should fail from test if same move is taken or self.draw_board is not updated" do
+      it 'should return argument itself' do
+        expect(object.my_move(move_valid)).to eq(move_valid)
+      end
+    end
+  end
 end
 
 describe Array do
@@ -47,14 +57,21 @@ describe Board do
     it "should display the board with user selection in 4 as 'X' " do
       positions = (1..9).to_a
       expect(Board.draw_board).to eq(
-        "
+    "
     #{positions[0]} | #{positions[1]} | #{positions[2]}
     =========
-    X | #{positions[4]} | #{positions[5]}
+    X | O | #{positions[5]}
     =========
     #{positions[6]} | #{positions[7]} | #{positions[8]}
     "
-      )
+    )
+  end
+end 
+
+  describe 'self.position_empty?' do
+    let(:move) {4}
+    it "Checking if position is emty, it should return false since 4 is taken" do
+      expect(Board.position_empty?(move)).to eq(false)
     end
   end
 end
