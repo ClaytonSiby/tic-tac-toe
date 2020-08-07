678cc81fbfd5de5a2528e25c78dc2fec0beaff9a
Visvaldas <visva.rapalis@gmail.com>
2020-08-07 17:38:59 +0300

    Fix rubocop errors

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -15,9 +15,9 @@ describe Player do
   end
 
   describe 'self.my_move' do
-    let(:object) {Player.new("Visvaldas" , "O")}
+    let(:object) { Player.new('Visvaldas', 'O') }
     let(:move_valid) { 5 }
-    context "it should fail from test if same move is taken or self.draw_board is not updated" do
+    context 'it should fail from test if same move is taken or self.draw_board is not updated' do
       it 'should return argument itself' do
         expect(object.my_move(move_valid)).to eq(move_valid)
       end
@@ -57,20 +57,20 @@ describe Board do
     it "should display the board with user selection in 4 as 'X' " do
       positions = (1..9).to_a
       expect(Board.draw_board).to eq(
-    "
+        "
     #{positions[0]} | #{positions[1]} | #{positions[2]}
     =========
     X | O | #{positions[5]}
     =========
     #{positions[6]} | #{positions[7]} | #{positions[8]}
     "
-    )
+      )
+    end
   end
-end 
 
   describe 'self.position_empty?' do
-    let(:move) {4}
-    it "Checking if position is emty, it should return false since 4 is taken" do
+    let(:move) { 4 }
+    it 'Checking if position is emty, it should return false since 4 is taken' do
       expect(Board.position_empty?(move)).to eq(false)
     end
   end
